96df22175b8d3e5dcd7848779dc478db72147263
gbaptista <gbaptistas@protonmail.com>
2020-08-23 08:58:40 -0300

    fixing workflow

--- .github/workflows/examples.sh
+++ .github/workflows/examples.sh
@@ -1,3 +1,3 @@
-bash _examples/lua/run.sh
-bash _examples/shell/run.sh
-bash _examples/fennel/run.sh
+bash _examples/lua/run.sh 1>/dev/null
+bash _examples/shell/run.sh 1>/dev/null
+bash _examples/fennel/run.sh 1>/dev/null
