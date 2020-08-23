b47192c7dad115c3c8d8577a4d964e23dbc8912a
gbaptista <gbaptistas@protonmail.com>
2020-08-23 08:50:13 -0300

    fixing workflow

---
+++ .github/workflows/examples.sh
@@ -0,0 +1,3 @@
+bash _examples/lua/run.sh
+bash _examples/shell/run.sh
+bash _examples/fennel/run.sh
--- .github/workflows/tests.sh
+++ .github/workflows/tests.sh
@@ -1,9 +1,5 @@
 luarocks install luaunit
 
-bash _examples/lua/run.sh
-bash _examples/shell/run.sh
-bash _examples/fennel/run.sh
-
 for file in **/**/*_test.lua; do
   lua "$file"
 done
